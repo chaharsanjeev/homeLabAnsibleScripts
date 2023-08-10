d8e0c7fffba7385fbbd6a0139ba1018803efe737
chaharsanjeev <41098385+chaharsanjeev@users.noreply.github.com>
2023-08-10 13:54:02 -0700

    Update cleanLinuxLogs.sh

--- cleanLinuxLogs.sh
+++ cleanLinuxLogs.sh
@@ -6,30 +6,19 @@
 # License: MIT
 # https://github.com/tteck/Proxmox/raw/main/LICENSE
 
-function header_info() {
-clear
-cat <<"EOF"
-   ________                    __   _  ________
-  / ____/ /__  ____ _____     / /  | |/ / ____/
- / /   / / _ \/ __ `/ __ \   / /   |   / /     
-/ /___/ /  __/ /_/ / / / /  / /___/   / /___   
-\____/_/\___/\__,_/_/ /_/  /_____/_/|_\____/   
-                                               
-EOF
-}
 BL=$(echo "\033[36m")
 GN=$(echo "\033[1;92m")
 CL=$(echo "\033[m")
 name=$(hostname)
-header_info
-echo -e "${BL}[Info]${GN} Cleaning $name${CL} \n"
+
+echo "${BL}[Info]${GN} Cleaning $name${CL} \n"
 cache=$(find /var/cache/ -type f)
 if [[ -z "$cache" ]]; then
-  echo -e "It appears there are no cached files on your system. \n"
+  echo "It appears there are no cached files on your system. \n"
   sleep 1
 else
-  echo -e "$cache \n"
-  echo -e "${GN}Cache in $name${CL}"
+  echo "$cache \n"
+  echo "${GN}Cache in $name${CL}"
   #read -p "Would you like to remove the selected cache listed above? [y/N] " -n 1 -r
   echo
   #if [[ $REPLY =~ ^[Yy]$ ]]; then
@@ -39,15 +28,15 @@ else
     sleep 1
   #fi
 fi
-# header_info
-echo -e "${BL}[Info]${GN} Cleaning $name${CL} \n"
+
+echo "${BL}[Info]${GN} Cleaning $name${CL} \n"
 logs=$(find /var/log/ -type f)
 if [[ -z "$logs" ]]; then
-  echo -e "It appears there are no logs on your system. \n"
+  echo  "It appears there are no logs on your system. \n"
   sleep 1
 else
-  echo -e "$logs \n"
-  echo -e "${GN}Logs in $name${CL}"
+  echo "$logs \n"
+  echo "${GN}Logs in $name${CL}"
   #read -p "Would you like to remove the selected logs listed above? [y/N] " -n 1 -r
   echo
   #if [[ $REPLY =~ ^[Yy]$ ]]; then
@@ -57,6 +46,6 @@ else
     sleep 1
   #fi
 fi
-#header_info
-echo -e "${BL}[Info]${GN} Cleaning $name${CL} \n"
-echo -e "${GN}Populating apt lists${CL} \n"
+
+echo  "${BL}[Info]${GN} Cleaning $name${CL} \n"
+echo  "${GN}Populating apt lists${CL} \n"
