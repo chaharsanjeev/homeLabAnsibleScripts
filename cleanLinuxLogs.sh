03880071590762aa7de78af8480bde8d8afeede9
chaharsanjeev <41098385+chaharsanjeev@users.noreply.github.com>
2023-08-10 13:38:09 -0700

    Update cleanLinuxLogs.sh

--- cleanLinuxLogs.sh
+++ cleanLinuxLogs.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+# Copyright (c) 2021-2023 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
+function header_info() {
+clear
+cat <<"EOF"
+   ________                    __   _  ________
+  / ____/ /__  ____ _____     / /  | |/ / ____/
+ / /   / / _ \/ __ `/ __ \   / /   |   / /     
+/ /___/ /  __/ /_/ / / / /  / /___/   / /___   
+\____/_/\___/\__,_/_/ /_/  /_____/_/|_\____/   
+                                               
+EOF
+}
 BL=$(echo "\033[36m")
 GN=$(echo "\033[1;92m")
 CL=$(echo "\033[m")
@@ -13,14 +29,14 @@ if [[ -z "$cache" ]]; then
 else
   echo -e "$cache \n"
   echo -e "${GN}Cache in $name${CL}"
-  # read -p "Would you like to remove the selected cache listed above? [y/N] " -n 1 -r
-  # echo
-  # if [[ $REPLY =~ ^[Yy]$ ]]; then
+  read -p "Would you like to remove the selected cache listed above? [y/N] " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo "Removing cache"
     find /var/cache -type f -delete
     echo "Successfully Removed cache"
     sleep 2
-  # fi
+  fi
 fi
 header_info
 echo -e "${BL}[Info]${GN} Cleaning $name${CL} \n"
@@ -31,15 +47,15 @@ if [[ -z "$logs" ]]; then
 else
   echo -e "$logs \n"
   echo -e "${GN}Logs in $name${CL}"
-  #read -p "Would you like to remove the selected logs listed above? [y/N] " -n 1 -r
+  read -p "Would you like to remove the selected logs listed above? [y/N] " -n 1 -r
   echo
-  #if [[ $REPLY =~ ^[Yy]$ ]]; then
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo "Removing logs"
     find /var/log -type f -delete
     echo "Successfully Removed logs"
     sleep 2
-  #fi
+  fi
 fi
-
+header_info
 echo -e "${BL}[Info]${GN} Cleaning $name${CL} \n"
 echo -e "${GN}Populating apt lists${CL} \n"
