644a6d7f3297c97a0c5c843caba7f714ac9e6209
chaharsanjeev <41098385+chaharsanjeev@users.noreply.github.com>
2023-10-09 10:41:50 -0700

    Update main_helper_script_update-lxcs.sh

--- Proxmox Helper Scripts/main_helper_script_update-lxcs.sh
+++ Proxmox Helper Scripts/main_helper_script_update-lxcs.sh
@@ -6,24 +6,6 @@
 
 #!/usr/bin/env bash
 
-rawurlencode() {
-  local string="${1}"
-  local strlen=${#string}
-  local encoded=""
-  local pos c o
-
-  for (( pos=0 ; pos<strlen ; pos++ )); do
-     c=${string:$pos:1}
-     case "$c" in
-        [-_.~a-zA-Z0-9] ) o="${c}" ;;
-        * ) printf -v o '%%%02x' "'$c"
-     esac
-     encoded+="${o}"
-  done
-  echo "${encoded}"    # You can either set a return variable (FASTER) 
-  REPLY="${encoded}"   #+or echo the result (EASIER)... or both... :p
-} # end function
-
 sendtelegram(){
   local data="Proxmox Server Below Proxmox Machines need restart since their Kernal was updated ${1}"
   echo "${data}"
@@ -32,6 +14,7 @@ sendtelegram(){
 
 
 sendtelegram "aaa"
+
 exit
 
 
