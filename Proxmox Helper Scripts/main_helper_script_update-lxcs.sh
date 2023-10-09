f5fcbec37c2eb5af1ea4c4f9ab6f06e14115f56c
chaharsanjeev <41098385+chaharsanjeev@users.noreply.github.com>
2023-10-09 10:25:09 -0700

    Update main_helper_script_update-lxcs.sh

--- Proxmox Helper Scripts/main_helper_script_update-lxcs.sh
+++ Proxmox Helper Scripts/main_helper_script_update-lxcs.sh
@@ -67,7 +67,7 @@ rawurlencode() {
 } # end function
 
 sendtelegram(){
-  local data="${1}"
+  local data= "Proxmox Server %0A%0A Below Proxmox MAchines need restart since their Kernal was updated %0A%0A ${1}"
   curl -k -X POST --connect-timeout 5 https://post.telegram.sc.home?msg=$( rawurlencode "$data" )
 } # end function
 
