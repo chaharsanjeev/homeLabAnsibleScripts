3ddd69ae4eedcb2cf68315b489ceba19f98de0c7
chaharsanjeev <41098385+chaharsanjeev@users.noreply.github.com>
2024-01-09 05:54:21 -0700

    Update getLinuxDetailsForHA.sh

--- Proxmox Helper Scripts/getLinuxDetailsForHA.sh
+++ Proxmox Helper Scripts/getLinuxDetailsForHA.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
 echo -e "\033[1;36msanju\033[0m"
+
+# get Linux machine boot details
+function getMostRecentBootDeails()
+{
+echo -e "\033[1;36mdddd\033[0m"
+
+
+
+}
+# #######################################################
+
+
+getMostRecentBootDeails
