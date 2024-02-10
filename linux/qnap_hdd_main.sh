de55f84b83b952492b4bf9362c7a2d6e9ddafbee
chaharsanjeev <41098385+chaharsanjeev@users.noreply.github.com>
2024-02-10 00:23:45 -0700

    Update qnap_hdd_main.sh

--- linux/qnap_hdd_main.sh
+++ linux/qnap_hdd_main.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-echo "hello world-3"
+echo "hello world-5"
 
 rm -f qnap_hdd_details.*
 
 wget "https://raw.githubusercontent.com/chaharsanjeev/homeLabAnsibleScripts/main/linux/qnap_hdd_details.py" -O "qnap_hdd_details.py"
-/share/CACHEDEV1_DATA/.system/Python python "qnap_hdd_details.py"
+
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin python "qnap_hdd_details.py"
