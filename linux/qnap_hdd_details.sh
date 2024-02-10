a69916fd732018b90203054be0e787656ad7545f
chaharsanjeev <41098385+chaharsanjeev@users.noreply.github.com>
2024-02-09 23:22:25 -0700

    Create qnap_hdd_details.sh

---
+++ linux/qnap_hdd_details.sh
@@ -0,0 +1,92 @@
+import os
+import json
+
+######### Variables #################
+# this program will get NAS HDD details and post these details to NODE-RED Server
+QNAP_UID = 'schahar'
+QNAP_PWD = 'Tuhina@0404'
+NODE_RED_ENPOINT_URL = 'http://nodered.sc:1880/nas_hdd_details'
+
+#####################################
+
+
+
+##################### common functions 
+def substring_after(s, delim):
+    return s.partition(delim)[2]
+######################## end functions
+
+
+class drive:
+    def __init__(self, name, totalsize,free,used,free_per):
+        self.name = name
+        self.totalsize = totalsize
+        self.free = free
+        self.used = used
+        self.free_per = free_per
+        
+########################################################
+
+    
+print('\nGet SID for QNAP')
+stream = os.popen('qcli -l user=' + QNAP_UID + ' pw=' + QNAP_PWD)
+output = stream.readlines()
+# print(output)
+
+# stip command to remove newline character and then convert array output to string 
+sid_string = ' '.join(str(x.strip()) for x in output) 
+
+# get sid from above response 
+sid_fetch = substring_after(sid_string, "sid is").strip()
+
+if sid_fetch.strip():
+   print('\nQNAP SID Found: "' + sid_fetch + '"')
+else:
+    print("\n------------------------------------------------------------------------------------------------------")
+    print("\nError Fetching SID : " + sid_string)
+    print("\n------------------------------------------------------------------------------------------------------\n\n\n")
+    exit()
+    
+# Now get the Volume Information 
+stream = os.popen('qcli_volume -i sid='+sid_fetch+' displayfield=Alias,Capacity,FreeSize')
+output = stream.readlines()
+# print(output)
+
+# remove first 3 items from the array since there are only headers
+del output[:3]
+
+print('\nWhat is the Type of Command Response: ' + type(output).__name__ + '\n')
+
+# creating list to store all drivers information
+lst = []
+
+for x in output:
+  row = x.strip().replace(' GB', '') # remove GB substring 
+  row = row[::-1] # reverse string 
+  list_of_strings = row.split()
+  #print(list_of_strings)
+  free_size = int(list_of_strings[0][::-1].strip()) # get first element and reverse string
+  totalsize = int(list_of_strings[1][::-1].strip()) # get first element and reverse string
+  used = totalsize - free_size
+  free_per = int(round((float(free_size) / float(totalsize))*100))
+  del list_of_strings[:2] #remove first two elements which is total capicity and free availabel size 
+  drv_name = (' '.join(str(x.strip()) for x in list_of_strings))[::-1].strip() # append remaining array element and string reverse it and trim it
+  lst.append(drive(name=drv_name, totalsize=totalsize,free=free_size, used=used, free_per=free_per )) 
+  
+  
+#Convert object to JSON String 
+json_string = json.dumps([ob.__dict__ for ob in lst])
+print('\nMessage to Send NODE-RED Below: \n')
+print(json_string)
+print('\n\n')
+print('\n\n')
+
+stream = os.popen('curl -X POST ' + ' "'+NODE_RED_ENPOINT_URL+'" -H "Content-Type: application/text" -d \'' + json_string + '\'')
+
+
+print('\n\n')
+#print(strr)
+print('\n\n')
+output = stream.readlines()
+#print('\n\n\n Response Details')
+#print(output)
