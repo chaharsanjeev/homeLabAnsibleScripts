621e734eabeda50bfbab276afedba8afb2fb7d3a
chaharsanjeev <41098385+chaharsanjeev@users.noreply.github.com>
2024-02-27 08:47:50 -0700

    Update getMachineDetails.sh

--- linux/getMachineDetails.sh
+++ linux/getMachineDetails.sh
@@ -45,7 +45,7 @@ function getMachineRAM(){
 function getMachineHDD
 {
     if [ "$HOSTNAME" = "backup" ]; then
-       COMMAND_OP=$(df --output=size,used,avail,target --total  --human-readable --block-size=1G -t ext4 /mnt/datastore/NAS-VM-Backups | head -n 2 | tail -1)
+       COMMAND_OP=$(df --output=size,used,avail,target --total  --human-readable --block-size=1M -t ext4 /mnt/datastore/NAS-VM-Backups | head -n 2 | tail -1)
        IFS=' ' read -a arr <<< "$COMMAND_OP" 
     else
        COMMAND_OP=$(df --output=size,used,avail --total  --human-readable --block-size=1M | head -n 2 | tail -1)
