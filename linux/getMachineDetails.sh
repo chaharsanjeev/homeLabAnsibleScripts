6bf2d90a351a48d07aea7004fc64fc5061562ea3
chaharsanjeev <41098385+chaharsanjeev@users.noreply.github.com>
2024-04-29 23:10:10 -0700

    Update getMachineDetails.sh

--- linux/getMachineDetails.sh
+++ linux/getMachineDetails.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 
-## Ensure to install the mysql client using the below
-## apt install default-mysql-client
+## Ensure to install the mysql client using the below apt install default-mysql-client
 declare -a MYSQL_HOST="db.sc"
 declare -a MYSQL_USERNAME="admin"
 declare -a MYSQL_PASSWORD="Tuhina@0404"
@@ -38,6 +37,11 @@ declare -a SYSTEM_UPTIME=""
 
 declare -a RECENT_APT_UPDATE_TIMESTAMP=""
 
+declare -a JSON_LINE=""
+declare -a JSON_ARRAY=""
+declare -a temp=""
+declare -a replace=""
+
 ############## GET RAM USAGE  #######################
 function getMachineRAM(){
       # COMMAND_OP=$(free --mega -h | head -n 2 | tail -1)
@@ -54,6 +58,8 @@ function getMachineRAM(){
 ################# GET HDD USAGE
 function getMachineHDD
 {
+     unset  JSON_ARRAY
+
     if [ "$HOSTNAME" = "backup" ]; then
        //COMMAND_OP=$(df --output=size,used,avail,target --total  --human-readable --block-size=1M -t ext4 /mnt/datastore/NAS-VM-Backups | head -n 2 | tail -1)
        COMMAND_OP=$(df --output=size,used,avail,target --total  --human-readable --block-size=1M -t ext4 /dev/mapper/pbs-root | head -n 2 | tail -1)
@@ -71,16 +77,48 @@ function getMachineHDD
     HDD_USED="${arr[1]}"
     HDD_FREE="${arr[2]}"
 
-     if [ "$HOSTNAME" = "backup" ]; then 
+     if [ "$HOSTNAME" = "backup" ]; then
            # get storage for backup server storage
            COMMAND_OP=$(df --output=size,used,avail,target --total  --human-readable --block-size=1M -t ext4 /mnt/datastore/NAS-VM-Backups | head -n 2 | tail -1)
-           IFS=' ' read -a arr <<< "$COMMAND_OP" 
+           IFS=' ' read -a arr <<< "$COMMAND_OP"
             //PBS_BACKUP_STORAGE_USAGE
             PVE_BACKUP_HDD_TOTAL="${arr[0]}"
             PVE_BACKUP_HDD_USED="${arr[1]}"
             PVE_BACKUP_HDD_FREE="${arr[2]}"
      elif [[ "$HOSTNAME" == "vm" ]]; then
            # get additional storage for VM
+
+          JSON_ARRAY=""
+
+	  while IFS= read -a oL ; do {  # reads single/one line
+	    BBB=($oL)
+	    temp="${BBB[3]}"
+            replace=""
+
+            temp="${temp/\/mnt\/pve\//$replace}"
+
+            JSON_LINE="{'totalsize': ${BBB[0]}, 'used': ${BBB[1]}, 'name': '${temp}', 'free': ${BBB[2]}}"
+
+            if [ -z "${JSON_ARRAY}" ]; then
+              JSON_ARRAY="[${JSON_LINE}"
+            else
+              JSON_ARRAY+=",${JSON_LINE}"
+            fi
+
+	  };
+
+	  done < <(df --output=size,used,avail,target --total  --human-readable --block-size=1G  /mnt/pve/* | tail -n +2);
+	  unset oL;
+
+	   if [ -n "${JSON_ARRAY}" ]; then
+               JSON_ARRAY+="]"
+           fi
+
+           # replace all single quotes with double quotes
+           # JSON_ARRAY="${JSON_ARRAY//\'/\"}"
+
+	   # echo  "${JSON_ARRAY}"
+
      fi 
     
 } # End Function
@@ -128,7 +166,7 @@ getAPTUpdateTimestamp
 
 # mosquitto_pub -h "${MQTT_HOST}" -p "${MQTT_PORT}" -u "${MQTT_UID}" -P "${MQTT_PWD}" --insecure -i "Linux_machine" -r -t "${MQTT_TOPIC}" -m "${json}"
 
-declare -a sql="UPDATE server_status SET PBS_BACKUP_STORAGE_TOTAL_MB= \"${PVE_BACKUP_HDD_TOTAL}\", PBS_BACKUP_STORAGE_USED_MB= \"${PVE_BACKUP_HDD_USED}\", KERNAL_NAME= \"${KERNAL_NAME}\" , RECENT_APT_UPDATE=\"${RECENT_APT_UPDATE_TIMESTAMP}\" , SYSTEM_UPTIME= \"${SYSTEM_UPTIME}\" , LAST_MODIFIED_DATE_TIME= CURRENT_TIMESTAMP, RAM_USED_MB=\"${RAM_USED}\" , RAM_TOTAL_MB=\"${RAM_TOTAL}\", SERVER_DNS=\"${HOST_NAME}\", HDD_TOTAL_MB=\"${HDD_TOTAL}\", HDD_USED_MB=\"${HDD_USED}\" WHERE SERVER_IP = \"${HOST_IP}\""
+declare -a sql="UPDATE server_status SET SERVER_MULTIPLE_DRIVE= \"${JSON_ARRAY}\", PBS_BACKUP_STORAGE_TOTAL_MB= \"${PVE_BACKUP_HDD_TOTAL}\", PBS_BACKUP_STORAGE_USED_MB= \"${PVE_BACKUP_HDD_USED}\", KERNAL_NAME= \"${KERNAL_NAME}\" , RECENT_APT_UPDATE=\"${RECENT_APT_UPDATE_TIMESTAMP}\" , SYSTEM_UPTIME= \"${SYSTEM_UPTIME}\" , LAST_MODIFIED_DATE_TIME= CURRENT_TIMESTAMP, RAM_USED_MB=\"${RAM_USED}\" , RAM_TOTAL_MB=\"${RAM_TOTAL}\", SERVER_DNS=\"${HOST_NAME}\", HDD_TOTAL_MB=\"${HDD_TOTAL}\", HDD_USED_MB=\"${HDD_USED}\" WHERE SERVER_IP = \"${HOST_IP}\""
 
 echo "Update SQL: ${sql}"
 
